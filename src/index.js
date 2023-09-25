c34350cc0deb304a325ab59c493d5b4b477194e8
CycleSDev <123fhghghg@gmail.com>
2023-09-25 12:48:02 +0300

    Start the project

---
+++ src/index.js
@@ -0,0 +1,8 @@
+require("file-loader?name=[name].[ext]!./index.html")
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
+
+const root = createRoot(document.getElementById("app"));
+
+root.render(<App />);
